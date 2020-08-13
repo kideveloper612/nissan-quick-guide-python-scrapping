90739efbc55e839fb55019f53914d18db9a14087
kideveloper612 <developer61510@gmail.com>
2020-08-13 18:53:11 +0800

    complete

--- nissanquickguide.py
+++ nissanquickguide.py
@@ -24,7 +24,7 @@ def get_driver(url):
         return driver
     except:
         time.sleep(1)
-        return get_driver()
+        return get_driver(url)
 
 
 def send_request(url):
@@ -38,15 +38,15 @@ def write(lines, file_name):
         writer.writerows(lines)
 
 
-def read():
-    with open(file='temp.csv', encoding='utf-8', mode='r') as csv_file:
+def read(file):
+    with open(file=file, encoding='utf-8', mode='r') as csv_file:
         rows = list(csv.reader(csv_file))
     return rows
 
 
 def main(year, model, model_url):
     driver = get_driver(url=model_url)
-    time.sleep(10)
+    time.sleep(5)
     topics = driver.find_elements_by_css_selector('ul.TopicsList a')
     sections = []
     topic_urls = []
@@ -86,23 +86,44 @@ def main(year, model, model_url):
         driver.quit()
 
 
-def detail():
-    records = read()
-    for record in records:
-        try:
-            driver = get_driver()
-            driver.get(record[4])
-            time.sleep(10)
-            title = driver.find_element_by_class_name('topic-title').text.strip()
-            video_url = driver.find_element_by_tag_name('video').get_attribute('src')
-            post_img = driver.find_element_by_class_name('vjs-poster').get_attribute('style').replace('background-image: url("', '').replace('");', '')
-            line = ['2020', 'Nissan', 'Sentra', record[3], title, video_url, post_img]
-            print(line)
-            write(lines=[line], file_name='Nissan_Sentra_2020_How_To.csv')
-        except NoSuchElementException:
-            print('No', record[4])
-        if driver:
-            driver.quit()
+def detail(year, temp_file):
+    if year == 2020:
+        records = read(temp_file)
+        for record in records:
+            try:
+                driver = get_driver(record[4])
+                time.sleep(10)
+                title = driver.find_element_by_class_name('topic-title').text.strip()
+                video_url = driver.find_element_by_tag_name('video').get_attribute('src')
+                post_img = driver.find_element_by_class_name('vjs-poster').get_attribute('style').replace('background-image: url("', '').replace('");', '')
+                line = [year, 'Nissan', record[2], record[3], title, video_url, post_img]
+                print(line)
+                write(lines=[line], file_name='Nissan_{}_How_To.csv'.format(year))
+            except NoSuchElementException:
+                print('No', record[4])
+            if driver:
+                driver.quit()
+    else:
+        records = read(temp_file)
+        count = 0
+        for record in records:
+            if count < 879:
+                count = count + 1
+                continue
+            try:
+                driver = get_driver(record[4])
+                time.sleep(10)
+                title = driver.find_element_by_class_name('topic-title').text.strip()
+                video_url = driver.find_element_by_tag_name('video').get_attribute('src')
+                post_img = driver.find_element_by_class_name('vjs-poster').get_attribute('style').replace(
+                    'background-image: url("', '').replace('");', '')
+                line = [year, 'Nissan', record[2], record[3], title, video_url, post_img]
+                print(line)
+                write(lines=[line], file_name='Nissan_{}_How_To.csv'.format(year))
+            except NoSuchElementException:
+                print('No', record[4])
+            if driver:
+                driver.quit()
 
 
 def read_html(filename):
@@ -112,17 +133,35 @@ def read_html(filename):
 
 def get_models():
     from bs4 import BeautifulSoup
-    for year in range(2014, 2021):
+    for year in range(2019, 2021):
         file = '{}.html'.format(year)
-        content = read_html(filename=file)
-        soup = BeautifulSoup(content, 'html5lib')
-        wrappers = soup.select('.group .list a')
-        for wrapper in wrappers:
-            model_name = wrapper.text.strip()
-            model_link = 'https://www.nissanquickguide.com/' + wrapper['href'].replace('explore', 'browse/folder/')
-            main()
+        temp_file = '{}.csv'.format(year)
+        if year == 2020:
+            content = read_html(filename=file)
+            soup = BeautifulSoup(content, 'html5lib')
+            wrappers = soup.select('.group .list a')
+            for wrapper in wrappers:
+                model_name = wrapper.text.strip()
+                model_link = 'https://www.nissanquickguide.com/' + wrapper['href'].replace('explore', 'browse/folder/')
+                main(year=year, model=model_name, model_url=model_link)
+            detail(year=year, temp_file=temp_file)
+        else:
+            detail(year=year, temp_file=temp_file)
+
+
+def gather():
+    records = []
+    for year in range(2014, 2021):
+        file_name = 'Nissan_{}_How_To.csv'.format(year)
+        rows = read(file=file_name)
+        for row in rows:
+            if row not in records:
+                print(row)
+                records.append(row)
+    write(lines=records, file_name='Nissan_How_To.csv')
+
+
 if __name__ == '__main__':
     base_url = 'https://www.nissanquickguide.com/#/guide/2020/sentra/browse/folder/'
-    main()
-    detail()
+    gather()
 
